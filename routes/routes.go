4b2033152ec99384e1046f5b3d7da347062c26da
Ian Guedes <ian.vgs@hotmail.com>
2023-04-22 17:29:14 -0300

    colocando coisas do dir

--- routes/routes.go
+++ routes/routes.go
@@ -2,15 +2,24 @@ package routes
 
 import (
 	"bmiz/controllers"
+	"log"
+	"os"
+	"path/filepath"
 
 	"github.com/gin-gonic/gin"
 )
 
 func HandleRequests() {
+
+	dir, err := filepath.Abs(filepath.Dir(os.Args[0]))
+	if err != nil {
+		log.Fatal("Erro ao carregar path")
+	}
+
 	r := gin.Default()
 
-	r.Static("/assets", "./assets")
-	r.LoadHTMLGlob("templates/*.html")
+	r.Static("/assets", dir+"/assets")
+	r.LoadHTMLGlob(dir + "/templates/*.html")
 
 	public := r.Group("/")
 	publicRoutes(public)
