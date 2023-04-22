84784b9c7a417fa15d8c725a6c0f4bb6d2626e7d
Ian Guedes <ian.vgs@hotmail.com>
2023-04-22 17:02:05 -0300

    .

--- main.go
+++ main.go
@@ -1,7 +1,28 @@
 package main
 
-import "bmiz/routes"
+import (
+	"bmiz/routes"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
 
 func main() {
+	if os.Getenv("GO_ENV") != "production" {
+		err := godotenv.Load()
+
+		if err != nil {
+			log.Fatal("Error loading dotenv file")
+		}
+
+		testdodotenv := os.Getenv("DOTENV")
+		log.Println(testdodotenv)
+
+	}
+
+	DOSERVICE := os.Getenv("DOSYSTEMDSERIVCE")
+	log.Println("DO SERVICE:", DOSERVICE)
+
 	routes.HandleRequests()
 }
